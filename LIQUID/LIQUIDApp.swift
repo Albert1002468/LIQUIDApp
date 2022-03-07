a800ade76636cd0dd590c28a346ce17ee8104afd
Albert1002468 <albert.d.mmi@gmail.com>
2022-03-07 15:07:05 -0600

    Initial Commit

---
+++ LIQUID/LIQUIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  LIQUIDApp.swift
+//  LIQUID
+//
+//  Created by Alberto Dominguez Fernandez on 3/7/22.
+//
+
+import SwiftUI
+
+@main
+struct LIQUIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
